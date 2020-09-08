12e51434e5fe805187e24e6a77470f96027bbc7e
ALEX-DESKTOP\AlexV <alexanderdvanhalen@gmail.com>
2020-09-08 12:56:15 -0500

    Initial card outline, title, fonts

---
+++ Assets/_Scripts/Card.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Card : MonoBehaviour
+{
+
+    //public enum CardType { Identity, Operation, Agenda, Ice, }
+
+
+    public string cardTitle;
+
+    [Header("Card References")]
+    public TextMeshProUGUI titleText;
+    public TextMeshProUGUI cardTypeText, cardSubTypeText;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+	//private void OnValidate()
+	//{
+ //       if (titleText) titleText.text = cardTitle;
+	//}
+
+
+}
